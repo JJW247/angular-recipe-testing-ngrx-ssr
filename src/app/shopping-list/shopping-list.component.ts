14dbd86a547f4ddbd73ea4cbd10f1d577a78ea11
jjw247 <adr10won@gmail.com>
2022-06-12 21:18:12 +0900

    Observable Pattern

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Subscription} from 'rxjs';
 import {Ingredient} from '../shared/ingredient.model';
 import {ShoppingListService} from './shopping-list.service';
 
@@ -7,15 +8,20 @@ import {ShoppingListService} from './shopping-list.service';
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.scss'],
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
+  private ingredientsChangeSub: Subscription;
 
   constructor(private shopingListService: ShoppingListService) {}
 
   ngOnInit(): void {
     this.ingredients = this.shopingListService.getIngredients();
-    this.shopingListService.ingredientsChanged.subscribe((ingredients: Ingredient[]) => {
+    this.ingredientsChangeSub = this.shopingListService.ingredientsChanged.subscribe((ingredients: Ingredient[]) => {
       this.ingredients = ingredients;
     });
+  }
+
+  ngOnDestroy(): void {
+    this.ingredientsChangeSub.unsubscribe();
   }
 }
