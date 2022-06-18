cbc656d110e088132a33ba1a2893063953344a81
jjw247 <adr10won@gmail.com>
2022-06-18 15:29:29 +0900

    reactive form applied

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -21,6 +21,10 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
     });
   }
 
+  onEditItem(index: number) {
+    this.shopingListService.startedEditing.next(index);
+  }
+
   ngOnDestroy(): void {
     this.ingredientsChangeSub.unsubscribe();
   }
