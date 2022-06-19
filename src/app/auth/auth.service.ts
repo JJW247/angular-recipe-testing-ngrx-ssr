bca5c63bf77348ce6fc0683284f4850071c4fd66
jjw247 <adr10won@gmail.com>
2022-06-19 14:14:59 +0900

    auth finished

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,8 @@
 import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import {Injectable} from '@angular/core';
-import {catchError, throwError} from 'rxjs';
+import {Router} from '@angular/router';
+import {BehaviorSubject, catchError, tap, throwError} from 'rxjs';
+import {User} from './user.model';
 
 export interface AuthResponseData {
   idToken: string;
@@ -10,7 +12,10 @@ export interface AuthResponseData {
 
 @Injectable({providedIn: 'root'})
 export class AuthService {
-  constructor(private http: HttpClient) {}
+  user = new BehaviorSubject<User>(null);
+  private tokenExpirationTimer: NodeJS.Timeout;
+
+  constructor(private http: HttpClient, private router: Router) {}
 
   signup(email: string, password: string) {
     return this.http
@@ -18,7 +23,12 @@ export class AuthService {
         email: email,
         password: password,
       })
-      .pipe(catchError(this.handleError));
+      .pipe(
+        catchError(this.handleError),
+        tap(responseData => {
+          this.handleAuthentication(responseData.email, responseData.idToken, +responseData.expiresIn);
+        }),
+      );
   }
 
   signin(email: string, password: string) {
@@ -27,7 +37,51 @@ export class AuthService {
         email: email,
         password: password,
       })
-      .pipe(catchError(this.handleError));
+      .pipe(
+        catchError(this.handleError),
+        tap(responseData => {
+          this.handleAuthentication(responseData.email, responseData.idToken, +responseData.expiresIn);
+        }),
+      );
+  }
+
+  autoLogin() {
+    const userData: {email: string; _token: string; _tokenExpirationDate: string} = JSON.parse(
+      localStorage.getItem('userData'),
+    );
+    if (!userData) {
+      return;
+    }
+    const loadedUser = new User(userData.email, userData._token, new Date(userData._tokenExpirationDate));
+    if (loadedUser.token) {
+      this.user.next(loadedUser);
+      const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
+      this.autoLogout(expirationDuration);
+    }
+  }
+
+  logout() {
+    this.user.next(null);
+    this.router.navigate(['/auth']);
+    localStorage.removeItem('userData');
+    if (this.tokenExpirationTimer) {
+      clearTimeout(this.tokenExpirationTimer);
+    }
+    this.tokenExpirationTimer = null;
+  }
+
+  autoLogout(expirationDuration: number) {
+    this.tokenExpirationTimer = setTimeout(() => {
+      this.logout();
+    }, expirationDuration);
+  }
+
+  private handleAuthentication(email: string, token: string, expiresIn: number) {
+    const tokenExpirationDate = new Date(expiresIn * 1000);
+    const user = new User(email, token, tokenExpirationDate);
+    this.user.next(user);
+    this.autoLogout(expiresIn * 1000);
+    localStorage.setItem('userData', JSON.stringify(user));
   }
 
   private handleError(errorResponse: HttpErrorResponse) {
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,26 @@
-import {Component} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Subscription} from 'rxjs';
+import {AuthService} from '../auth/auth.service';
 import {DataStorageService} from '../shared/data-storage.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
+  isAuthenticated = false;
+  private userSubscription: Subscription;
+
   // responsive display for tiny viewport
   collapsed: boolean = true;
 
-  constructor(private dataStorageService: DataStorageService) {}
+  constructor(private dataStorageService: DataStorageService, private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.userSubscription = this.authService.user.subscribe(user => {
+      this.isAuthenticated = !!user;
+    });
+  }
 
   onSaveData() {
     this.dataStorageService.storeRecipes();
@@ -17,5 +28,13 @@ export class HeaderComponent {
 
   onFetchData() {
     this.dataStorageService.fetchRecipes().subscribe();
+  }
+
+  onLogout() {
+    this.authService.logout();
+  }
+
+  ngOnDestroy(): void {
+    this.userSubscription.unsubscribe();
   }
 }
